0d34ea641bf7bbcc170e2b7fb83e382372b5ab37
petermr <peter.murray.rust@googlemail.com>
2024-09-13 09:25:30 +0100

    added alis2024/dot

---
+++ alis2024/dot/all1_dot.sh
@@ -0,0 +1,15 @@
+
+dot -Tsvg args/prenorma.dot -oargs/prenorma.svg
+dot -Tsvg asgt/math7.dot -oasgt/math7.svg
+dot -Tsvg assets/dependencies.dot -oassets/dependencies.svg
+dot -Tsvg assets/dependencies2.dot -oassets/dependencies2.svg
+dot -Tsvg climate/dependencies.dot -oclimate/dependencies.svg
+dot -Tsvg docanalysis/dependencies.dot -odocanalysis/dependencies.svg
+dot -Tsvg dotty/poms.dot -odotty/poms.svg
+dot -Tsvg images/graph.dot -oimages/graph.svg
+dot -Tsvg norma/norma.dot -onorma/norma.svg
+dot -Tsvg norma/normasmall.dot -onorma/normasmall.svg
+dot -Tsvg phylotree/ami.dot -ophylotree/ami.svg
+dot -Tsvg plugins/ami.dot -oplugins/ami.svg
+dot -Tsvg pyamiimage/dependencies.dot -opyamiimage/dependencies.svg
+dot -Tsvg resources/test.dot -oresources/test.svg
